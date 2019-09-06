899e9a88da181ca06409ce68ddfa1ff2304b7936
dianlujitao <dianlujitao@lineageos.org>
2019-09-06 11:19:55 +0800

    display: Use generated kernel headers
    Change-Id: I7cdd8c2ac14597ed0ddd458de5a6ee9ceff89380

--- gralloc/Android.mk
+++ gralloc/Android.mk
@@ -19,12 +19,11 @@ LOCAL_C_INCLUDES              += external/libcxx/include \
 LOCAL_SHARED_LIBRARIES        += libion
 LOCAL_CFLAGS                  += -std=c++14
 endif
-LOCAL_HEADER_LIBRARIES        := display_headers
+LOCAL_HEADER_LIBRARIES        := display_headers generated_kernel_headers
 ifneq ($(TARGET_KERNEL_VERSION), 4.14)
-LOCAL_CFLAGS                  += -isystem  $(kernel_includes)
+LOCAL_CFLAGS                  += -isystem
 endif
 LOCAL_CLANG                   := true
-LOCAL_ADDITIONAL_DEPENDENCIES := $(common_deps) $(kernel_deps)
 LOCAL_SRC_FILES               := gr_ion_alloc.cpp \
                                  gr_allocator.cpp \
                                  gr_buf_mgr.cpp \
@@ -43,14 +42,13 @@ include $(CLEAR_VARS)
 LOCAL_MODULE                  := libgrallocutils
 LOCAL_VENDOR_MODULE           := true
 LOCAL_MODULE_TAGS             := optional
-LOCAL_C_INCLUDES              := $(common_includes) $(kernel_includes)
+LOCAL_C_INCLUDES              := $(common_includes)
 ifeq ($(TARGET_KERNEL_VERSION), 4.14)
 LOCAL_C_INCLUDES              += system/core/libion/include \
                                  system/core/libion/kernel-headers
 endif
-LOCAL_HEADER_LIBRARIES        := display_headers
+LOCAL_HEADER_LIBRARIES        := display_headers generated_kernel_headers
 LOCAL_SHARED_LIBRARIES        := $(common_libs) libqdMetaData libdl android.hardware.graphics.common@1.1
 LOCAL_CFLAGS                  := $(common_flags) -DLOG_TAG=\"grallocutils\" -Wno-sign-conversion
-LOCAL_ADDITIONAL_DEPENDENCIES := $(common_deps) $(kernel_deps)
 LOCAL_SRC_FILES               := gr_utils.cpp gr_adreno_info.cpp
 include $(BUILD_SHARED_LIBRARY)
